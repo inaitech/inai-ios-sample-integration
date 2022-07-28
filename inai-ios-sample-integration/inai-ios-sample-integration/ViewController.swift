c82f3f5c2a5e58fbe470e34852045004fd96b731
Amit Yadav <amit.y@inai.io>
2022-07-29 00:14:23 +0530

    Project Scaffolding files added

---
+++ inai-ios-sample-integration/inai-ios-sample-integration/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  inai-ios-sample-integration
+//
+//  Created by Amit Yadav on 28/07/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
