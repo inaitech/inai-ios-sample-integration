a737d5b78037548f86b99c5301fdd55002c8ac12
Amit Yadav <amit.y@inai.io>
2022-09-05 15:18:32 +0530

    Added DropIn checkout feature from Inai SDK

---
+++ inai-ios-sample-integration/DropIn Checkout/DropInCheckoutViewController.swift
@@ -0,0 +1,203 @@
+//
+//  DropInCheckoutViewController.swift
+//  inai-ios-sample-integration
+//
+//  Created by Amit Yadav on 05/09/22.
+//
+
+import UIKit
+import inai_ios_sdk
+
+class DropInCheckoutViewController: UIViewController {
+    
+    // MARK: - Internal vars
+    // MARK: -
+    @IBOutlet weak var activityIndicator: UIActivityIndicatorView!
+    var orderId: String = ""
+    
+    var base_url: String! {
+        return PlistConstants.shared.baseURL
+    }
+    
+    var inai_prepare_order_url: String! {
+        return "\(base_url!)/orders"
+    }
+    
+    // MARK: - Internal Methods
+    // MARK: -
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+    
+    @IBAction func clickedBuyNow(_ sender: Any) {
+       prepareOrderAndCheckout()
+    }
+    
+    private func prepareOrderAndCheckout() {
+        //  Initiate a new order
+        self.activityIndicator.startAnimating()
+        
+        //  Prep postdata
+        let Customer_ID_Key = "customerId-\(PlistConstants.shared.token)"
+        var savedCustomerId: String? = UserDefaults.standard.string(forKey: Customer_ID_Key) ?? nil
+
+        var body: [String: AnyHashable] = [
+            "amount": PlistConstants.shared.amount,
+            "currency": PlistConstants.shared.currency,
+            "description": "Acme Shirt",
+            "metadata": ["test_order_id": "5735"]
+        ]
+        
+        if let savedCustomerId = savedCustomerId {
+            //  Lets reuse the existing customer
+            body["customer"] =  [ "id": savedCustomerId]
+        } else {
+            //  Create a new customer
+            body["customer"] = ["email": "testdev@test.com",
+                                 "first_name": "Dev",
+                                 "last_name": "Smith",
+                                 "contact_number": "01010101010"]
+        }
+
+        self.request(url: URL(string: self.inai_prepare_order_url)!,
+                     method: "POST",
+                     postData: body) { (data, error) in
+            
+            var orderId: String? = nil
+            
+            if let error = error {
+                print(error.localizedDescription)
+            }
+            else {
+                if let data = data {
+                    if let responseOrderId = data["id"] as? String {
+                        orderId = responseOrderId
+                    }
+                    
+                    if savedCustomerId == nil {
+                        if let customerId = data["customer_id"] as? String {
+                            //  Save customer id to defaults so we can reuse it
+                            savedCustomerId = customerId
+                            UserDefaults.standard.set(customerId, forKey: Customer_ID_Key)
+                        }
+                    }
+                }
+            }
+             
+            self.activityIndicator.stopAnimating()
+            if let orderId = orderId {
+                self.orderId = orderId
+                self.checkout()
+            }
+        }
+    }
+    
+    private func request(url: URL, method: String,
+                         postData: [String: Any]?,
+                         completion: @escaping ([String: Any]?, Error?) -> Void) {
+        
+        let authStr = "\(PlistConstants.shared.token):\(PlistConstants.shared.password)"
+        let encodedAuthStr = "BASIC \(Data(authStr.utf8).base64EncodedString())"
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = method
+        
+        //  Set headers
+        request.setValue(encodedAuthStr, forHTTPHeaderField: "Authorization")
+        request.setValue("application/json", forHTTPHeaderField: "Accept")
+        request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        //  Attach postdata if applicable
+        if let postData = postData {
+            request.httpBody = try? JSONSerialization.data(withJSONObject: postData, options: .fragmentsAllowed)
+        }
+        
+        let task = URLSession.shared.dataTask(with: request) { data, urlRequest, error in
+            var result: [String: Any]? = nil
+            if let data = data {
+                let json = try? JSONSerialization.jsonObject(with: data, options: .fragmentsAllowed)
+                result = json as? [String : Any]
+            }
+            
+            DispatchQueue.main.async {
+                completion(result, error)
+            }
+        }
+        //  Fire away
+        task.resume()
+    }
+    
+    private func checkout() {
+        let styles = InaiConfig_Styles(
+            container: InaiConfig_Styles_Container(backgroundColor: "#fff"),
+            cta: InaiConfig_Styles_Cta(backgroundColor: "#123456"),
+            errorText: InaiConfig_Styles_ErrorText(color: "#000000")
+        )
+        
+        let config = InaiConfig(token: PlistConstants.shared.token,
+                                orderId : self.orderId,
+                                styles: styles,
+                                countryCode: PlistConstants.shared.country
+        )
+        
+        if let inaiCheckout = InaiCheckout(config: config) {
+            inaiCheckout.presentCheckout(viewController: self, delegate: self)
+        }
+    }
+}
+
+extension DropInCheckoutViewController: InaiCheckoutDelegate {
+    
+    private func goToHomeScreen(action: UIAlertAction) -> Void {
+        self.navigationController?.popToRootViewController(animated: true)
+    }
+    
+    func paymentFinished(with result: Inai_PaymentResult) {
+        switch result.status {
+        case Inai_PaymentStatus.success:
+            let resultStr = convertDictToStr(result.data)
+            self.showAlert("Payment Success! \(resultStr)", title: "Result", completion: goToHomeScreen)
+            break
+            
+        case Inai_PaymentStatus.failed:
+            let strError = convertDictToStr(result.data)
+            self.showAlert("Payment Failed with data: \(strError)", title: "Result", completion: goToHomeScreen)
+            break
+            
+        case Inai_PaymentStatus.canceled:
+            let message = result.data["message"] ?? "Payment Canceled!"
+            self.showAlert(message as! String, title: "Result", completion: goToHomeScreen)
+            break
+        @unknown default:
+            break;
+        }
+    }
+    
+    func showAlert(_ message: String, title: String = "Alert", completion: ((UIAlertAction) -> Void)? = nil ) {
+        let alert = UIAlertController(title: title, message: message, preferredStyle: .alert)
+        alert.addAction(UIAlertAction(title: "OK", style: UIAlertAction.Style.default, handler: completion))
+        self.present(alert, animated: true, completion: nil)
+    }
+    
+    func convertDictToStr(_ dict: [String:Any]) -> String {
+        var jsonStr = "";
+        do {
+            let jsonData = try JSONSerialization.data(withJSONObject: dict, options: .fragmentsAllowed)
+            jsonStr = String(data: jsonData, encoding: String.Encoding.utf8) ?? ""
+        } catch {}
+        
+        return jsonStr
+    }
+    
+    func convertStrToDict(_ text: String) -> [String: Any]? {
+        if let data = text.data(using: .utf8) {
+            do {
+                return try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any]
+            } catch {
+                print(error.localizedDescription)
+            }
+        }
+        return nil
+    }
+}
+
